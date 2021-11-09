52ec456bd1fe8a3ef406eeef867f03603fa16d98
Your Name <you@example.com>
2021-11-09 21:45:09 +0800

    Update on 2021/11/9

--- backEnd/src/main/java/com/example/demo/controller/orderController.java
+++ backEnd/src/main/java/com/example/demo/controller/orderController.java
@@ -1,10 +1,12 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.order.OrderVo;
+import com.example.demo.utils.ResponseCode;
+import com.example.demo.utils.ReturnObject;
 import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Objects;
+import java.util.List;
 
 
 @RestController
@@ -13,86 +15,84 @@ import java.util.Objects;
 @CrossOrigin
 public class orderController {
     //面向用户
-    @GetMapping("/user")
+    @GetMapping("/user/orders")
     @ApiOperation(value="面向用户：查询用户名下所有订单")
-    public String getOrders(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<List<OrderVo>> getOrders(@RequestHeader(name="authorization")String auth,
                             @RequestParam("state")String state) {
-        return state;
+        return new ReturnObject<List<OrderVo>>();
     }
-    @PostMapping("/user")
+    @PostMapping("/user/orders")
     @ApiOperation(value="面向用户：用户提交订单")
-    public String submitNewOrder(@RequestHeader(name="authorization")String auth,
-                                 @RequestBody OrderVo order) {
-        return order.toString();
+    public ReturnObject<List<OrderVo>> submitNewOrder(@RequestHeader(name="authorization")String auth,
+                                       @RequestBody OrderVo order) {
+        return new ReturnObject<List<OrderVo>>();
     }
-    @GetMapping("/user/{id}")
+    @GetMapping("/user/orders/{id}")
     @ApiOperation(value="面向用户：用户通过订单id查询特定订单详细信息")
-    public String getCertainOrder(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<List<OrderVo>> getCertainOrder(@RequestHeader(name="authorization")String auth,
                                   @PathVariable(name="id") String id){
-        return id;
+        return new ReturnObject<List<OrderVo>>();
     }
-    @PutMapping("/user/{id}")
+    @PutMapping("/user/orders/{id}")
     @ApiOperation(value="面向用户：用户通过订单id修改特定订单详细信息")
-    public String alterCertainOrder(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<String> alterCertainOrder(@RequestHeader(name="authorization")String auth,
                                     @PathVariable String id,@RequestBody OrderVo order){
-        return id;
+        return new ReturnObject<String>();
     }
-    @DeleteMapping("/user/{id}")
+    @DeleteMapping("/user/orders/{id}")
     @ApiOperation(value="面向用户：用户通过订单id删除特定订单详细信息")
-    public String delCertainOrder(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<String> delCertainOrder(@RequestHeader(name="authorization")String auth,
                                   @PathVariable(name="id") String id){
-        return id;
+        return new ReturnObject<String>();
     }
-    @PutMapping("/user/{id}/confirm")
+    @PutMapping("/user/orders/{id}/confirm")
     @ApiOperation(value="面向用户：用户通过订单id修改名下订单状态为确认收货")
-    public String confirmOrders(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<String> confirmOrders(@RequestHeader(name="authorization")String auth,
                                 @PathVariable(name="id") String id){
-        return id;
+        return new ReturnObject<String>();
     }
-    @PutMapping("/user/{id}/groupon-normal")
+    @PutMapping("/user/orders/{id}/groupon-normal")
     @ApiOperation(value="面向用户：用户通过订单id修改名下团购订单为普通订单")
-    public String grouponToNormal(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<String> grouponToNormal(@RequestHeader(name="authorization")String auth,
                                   @PathVariable(name="id") String id){
-        return id;
+        return new ReturnObject<String>();
     }
     //面向商铺
     @GetMapping("/shop/{shopId}/orders")
     @ApiOperation(value="面向商铺：查询商铺名下所有订单")
-    public String getShopOrders(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<List<OrderVo>> getShopOrders(@RequestHeader(name="authorization")String auth,
                                 @PathVariable(name="shopId") String shopId,
                                 @RequestParam(name="customerId")String customerId,
                                 @RequestParam(name="state")String state){
-        return "Get it";
+        return new ReturnObject<List<OrderVo>>();
     }
     @GetMapping("/shop/{shopId}/orders/{id}")
     @ApiOperation(value="面向商铺：店铺根据订单id查询名下特定订单详细信息")
-    public String getCertainShopOrder(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<List<OrderVo>> getCertainShopOrder(@RequestHeader(name="authorization")String auth,
                                       @PathVariable(name="shopId") String shopId,
-                                      @PathVariable(name="id") String id)
-    {
-        return "Get it";
+                                      @PathVariable(name="id") String id) {
+        return new ReturnObject<List<OrderVo>>();
     }
     @PutMapping("/shop/{shopId}/orders/{id}")
     @ApiOperation(value="面向商铺：店铺根据订单id修改名下特定订单详细信息")
-    public String alterCertainShopOrder(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<String> alterCertainShopOrder(@RequestHeader(name="authorization")String auth,
                                         @PathVariable(name="shopId") String shopId,
-                                        @PathVariable(name="id") String id)
-    {
-        return "Get it";
+                                        @PathVariable(name="id") String id) {
+        return new ReturnObject<String>();
     }
     @DeleteMapping("/shop/{shopId}/orders/{id}")
     @ApiOperation(value="面向商铺：店铺根据订单id删除订单")
-    public String delCertainShopOrder(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<String> delCertainShopOrder(@RequestHeader(name="authorization")String auth,
                                       @PathVariable(name="shopId") String shopId,
                                       @PathVariable(name="id") String id)
     {
-        return "Get it";
+        return new ReturnObject<String>();
     }
     @PutMapping("/shop/{shopId}/orders/{id}/deliver")
     @ApiOperation(value="面向商铺：商铺根据订单id标记名下订单已发货")
-    public String delivered(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<String> delivered(@RequestHeader(name="authorization")String auth,
                             @PathVariable(name="shopId") String shopId,
                             @PathVariable(name="id") String id){
-        return "Get it";
+        return new ReturnObject<String>();
     }
 }
--- backEnd/src/main/java/com/example/demo/controller/paymentController.java
+++ backEnd/src/main/java/com/example/demo/controller/paymentController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.demo.model.payment.PaymentVo;
 import com.example.demo.model.refund.RefundVo;
+import com.example.demo.utils.ResponseCode;
+import com.example.demo.utils.ReturnObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
@@ -13,86 +16,97 @@ import org.springframework.web.bind.annotation.*;
 public class paymentController {
     @GetMapping("/states")
     @ApiOperation(value="获取所有支付单的支付状态")
-    public String getPaymentsState(){
-        return "Get it";
+    public ReturnObject<PaymentVo> getPaymentsState(){
+        return new ReturnObject<PaymentVo>();
     }
     @GetMapping("/patterns")
     @ApiOperation(value="获取所有的支付渠道")
-    public String getPaymentsPatterns(){
-        return "Get it";
+    public Object getPaymentsPatterns(){
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("001","积点支付");
+        jsonObject.put("002","模拟渠道支付");
+        return jsonObject;
     }
-    @PostMapping("/orders/{id}/payments")
+    @PostMapping("/user/orders/{id}")
     @ApiOperation(value="面向买家：买家为订单创建支付单")
-    public String createPayment(@RequestHeader(name="authorization")String auth,
-                                @PathVariable(name="id")String id,
-                                @RequestBody PaymentVo payment) {
-        return "Get it";
+    public ReturnObject<ResponseCode> createPayment(@RequestHeader(name="authorization")String auth,
+                                                    @PathVariable(name="id")String id,
+                                                    @RequestBody PaymentVo payment) {
+        return new ReturnObject<ResponseCode>();
     }
-    @GetMapping("/orders/{id}/payments")
+    @GetMapping("/user/orders/{id}")
     @ApiOperation(value="面向买家：买家根据订单id获取支付信息")
-    public String createPayment(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<PaymentVo> createPayment(@RequestHeader(name="authorization")String auth,
                                 @PathVariable(name="id")String id) {
-        return "Get it";
+        return new ReturnObject<PaymentVo>();
     }
     @GetMapping("/shop/{shopId}/orders/{id}/payments")
     @ApiOperation(value="面向商铺：商铺根据商铺id和订单id查询自己名下订单的支付信息")
-    public String getShopCertainOrderPayment(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<PaymentVo> getShopCertainOrderPayment(@RequestHeader(name="authorization")String auth,
                                              @PathVariable(name="shopId")String shopId,
                                              @PathVariable(name="id")String id){
-        return "Get it";
+        return new ReturnObject<PaymentVo>();
     }
-    @PostMapping("/aftersales/{id}/payments")
+    @PostMapping("/user/aftersales/{id}/payments")
     @ApiOperation(value="面向买家：买家为售后订单创建支付单")
-    public String getAftersalesPayments(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<ResponseCode> getAftersalesPayments(@RequestHeader(name="authorization")String auth,
                                         @PathVariable(name="id")String id,
                                         @RequestBody PaymentVo payment){
-        return "Get it";
+        return new ReturnObject<ResponseCode>();
     }
-    @GetMapping("/aftersales/{id}/payments")
+    @GetMapping("/user/aftersales/{id}/payments")
     @ApiOperation(value="面向买家：买家根据售后订单id查询支付单")
-    public String getAftersalesPayments(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<PaymentVo> getAftersalesPayments(@RequestHeader(name="authorization")String auth,
                                         @PathVariable(name="id")String id){
-        return "Get it";
+        return new  ReturnObject<PaymentVo>();
     }
     @GetMapping("/shops/{shopId}/aftersales/{id}/payments")
     @ApiOperation(value="面向商铺：商铺根据商铺id和售后订单id查询名下的售后订单支付信息")
-    public String getShopAfterSalePayments(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<PaymentVo> getShopAfterSalePayments(@RequestHeader(name="authorization")String auth,
                                            @PathVariable(name="shopId")String shopId,
                                            @PathVariable(name="id")String id){
-        return "Get it";
+        return new ReturnObject<PaymentVo>();
     }
     @PostMapping("/shops/{shopId}/payments/{id}/refunds")
     @ApiOperation(value="面向商铺：商铺根据商铺id和支付id，对自己名下的已支付支付单发起退款")
-    public String submitRefund(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<ResponseCode> submitRefund(@RequestHeader(name="authorization")String auth,
                                            @PathVariable(name="shopId")String shopId,
                                            @PathVariable(name="id")String id,
                                            @RequestBody RefundVo refund){
-        return "Get it";
+        return new ReturnObject<ResponseCode>();
     }
     @GetMapping("/shops/{shopId}/orders/{id}/refunds")
     @ApiOperation(value="面向商铺：商铺根据商铺id和订单id查询名下退款信息")
-    public String getOrderRefund(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<PaymentVo> getOrderRefund(@RequestHeader(name="authorization")String auth,
                                            @PathVariable(name="shopId")String shopId,
                                            @PathVariable(name="id")String id){
-        return "Get it";
+        return new ReturnObject<PaymentVo>();
     }
     @GetMapping("/shops/{shopId}/aftersales/{id}/refunds")
     @ApiOperation(value="面向商铺：商铺根据商铺id和售后单id查询名下退款信息")
-    public String getAfterSaleRefund(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<PaymentVo> getAfterSaleRefund(@RequestHeader(name="authorization")String auth,
                             @PathVariable(name="shopId")String shopId,
                             @PathVariable(name="id")String id){
-        return "Get it";
+        return new ReturnObject<PaymentVo>();
     }
-    @GetMapping("/orders/{id}/refunds")
+    @GetMapping("/user/orders/{id}/refunds")
     @ApiOperation(value="面向买家：买家根据自己名下的订单id查询退款信息")
-    public String getOrderRefundsForCust(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<PaymentVo> getOrderRefundsForCust(@RequestHeader(name="authorization")String auth,
                                     @PathVariable(name="id")String id){
-        return "Get it";
+        return new ReturnObject<PaymentVo>();
     }
-    @GetMapping("/aftersales/{id}/refunds")
+    @GetMapping("/user/aftersales/{id}/refunds")
     @ApiOperation(value="面向买家：买家根据自己名下的售后单id查询退款信息")
-    public String getAftersaleRefundsForCust(@RequestHeader(name="authorization")String auth,
+    public ReturnObject<PaymentVo> getAftersaleRefundsForCust(@RequestHeader(name="authorization")String auth,
                                     @PathVariable(name="id")String id){
-        return "Get it";
+        return new ReturnObject<PaymentVo>();
+    }
+    @PostMapping("/shop/{shopId}/deposit")
+    @ApiOperation(value="面向商铺：卖家根据商铺id缴纳保证金")
+    public ReturnObject<PaymentVo> submitShopDepositPayment(@RequestHeader(name="authorization")String auth
+                                                            ,@RequestParam(name="amount")double amount){
+        PaymentVo paymentVo=new PaymentVo();
+        paymentVo.setDueAmount(amount);
+        return new ReturnObject<PaymentVo>(paymentVo);
     }
 }
